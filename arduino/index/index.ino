91b7b5e9b9a54b2818ccec7fd703d2aae9398663
Kaleb <kalebjean22@gmail.com>
2021-12-04 16:10:27 -0300

    Finalizado teste com o sensor de humidade e a conexao com o Broker

--- arduino/index/index.ino
+++ arduino/index/index.ino
@@ -1,56 +1,47 @@
 #include <Ethernet.h>
-#include <SPI.h>
- 
+#include <PubSubClient.h>
+
 byte mac[] = {  0x90, 0xA2, 0xDA, 0x0D, 0xF6, 0xFF }; 
-byte ip[] = {  192, 168, 15, 182 };
-// byte gateway[] = { x,x,x,x };      
+byte ip[] = {  192, 168, 15, 189 };
 
-EthernetClient client;
+EthernetClient ethClient;
+PubSubClient mqttClient(ethClient);
 
-String temp= "data=5";  
+const int pinoSensor = A7; //PINO UTILIZADO PELO SENSOR
+int valorLido; //VARIÁVEL QUE ARMAZENA O PERCENTUAL DE UMIDADE DO SOLO
+int erroMqtt;
+String msg;
+char msgTemp[50];
+
+int analogSoloSeco = 400; //VALOR MEDIDO COM O SOLO SECO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
+int analogSoloMolhado = 150; //VALOR MEDIDO COM O SOLO MOLHADO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
+int percSoloSeco = 0; //MENOR PERCENTUAL DO SOLO SECO (0% - NÃO ALTERAR)
+int percSoloMolhado = 100; //MAIOR PERCENTUAL DO SOLO MOLHADO (100% - NÃO ALTERAR)
 
 void setup(){
-
-  Ethernet.begin(mac, ip);
-  Serial.begin(9600);
-  Serial.println(Ethernet.localIP());
-  delay(1000);
-  delay(1000);
-  Serial.println("connecting...");
-  
-  if (client.connect("192.168.15.181",3000))
-  {                                 
-    Serial.println("Sending to Server: ");                    
-    client.println("POST /teste HTTP/1.1");           
-    Serial.print("POST /teste HTTP/1.1");
-    client.println("Host: 192.168.15.181");
-    client.println("Content-Type: application/x-www-form-urlencoded");
-    client.println("Connection: close");
-    client.println("User-Agent: Arduino/1.0");
-    client.print("Content-Length: ");
-    client.println(temp.length());
-    client.println();
-    client.print(temp);
-    client.println();                                           
-  }else{
-    Serial.println("Cannot connect to Server");               
-  }
-
+ mqttClient.setServer("192.168.15.181", 1883);
+ Ethernet.begin(mac, ip);
+ Serial.begin(9600); //INICIALIZA A SERIAL
+ Serial.println(Ethernet.localIP());
+ mqttClient.connect("arduino");
+ delay(5000); //INTERVALO DE 5 SEGUNDOS
+ Serial.println("Lendo a umidade do solo..."); //IMPRIME O TEXTO NO MONITOR SERIAL
 }
 
 void loop(){
+ valorLido = constrain(analogRead(pinoSensor),analogSoloMolhado,analogSoloSeco); //MANTÉM valorLido DENTRO DO INTERVALO (ENTRE analogSoloMolhado E analogSoloSeco)
+ valorLido = map(valorLido,analogSoloMolhado,analogSoloSeco,percSoloMolhado,percSoloSeco); //EXECUTA A FUNÇÃO "map" DE ACORDO COM OS PARÂMETROS PASSADOS
+ Serial.print("Umidade do solo: "); //IMPRIME O TEXTO NO MONITOR SERIAL
+ Serial.print(valorLido); //IMPRIME NO MONITOR SERIAL O PERCENTUAL DE UMIDADE DO SOLO
+ Serial.println("%"); //IMPRIME O CARACTERE NO MONITOR SERIAL
+ msg = String(valorLido);
+ msg.toCharArray(msgTemp, msg.length() + 1);
+ if(mqttClient.publish("temperatura", msgTemp)){
+  Serial.println("Mensagem enviada ao Broker");
+ }else{
+  erroMqtt = mqttClient.state();
+  Serial.println(erroMqtt);
+ }
 
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
-
-  if (!client.connected()){
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-    for(;;)
-      ;
-  }
-
+ delay(1000);  //INTERVALO DE 1 SEGUNDO
 }
--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,26 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+const mqtt = require('mqtt')
+
+const client = mqtt.connect('mqtt://localhost', {
+  clientId: 'nestjs',
+  clean: true,
+  connectTimeout: 8000,
+  username: '',
+  password: '',
+  reconnectPeriod: 1000,
+})
+
+client.on('connect', () => {
+  console.log('Connected')
+  client.subscribe(['temperatura'], () => {
+    console.log(`Subscribe to topic 'temperatura'`)
+  })
+})
+
+client.on('message', (topic, payload) => {
+  console.log('Temperatura do solo:', payload.toString(), '%')
+})
 
 @Controller()
 export class AppController {
